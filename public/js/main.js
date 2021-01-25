763bd741f55e8683077c8e3bcbd4e4bf30e5c22a
zulma <zumendez7@gmail.com>
2021-01-25 21:52:15 +0100

    modification de la methode takecare

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,28 @@
 // ​
 // ## Description des patients
 // >les malades ont un nom, une maladie, de l'argent, une poche, un état de santé,ils savent aller à un endroit, prendre un médicamment et payer. Au début, les patients sont dans un salle d'attente. 
+class Pharmacie {
+    constructor(traitement, prix){
+        this.traitement = traitement;
+        this.prix = prix; 
+    }
+}
+
 class Malades {
-    constructor(nom, maladie, argent, poche, etatSante, traitement){
+    constructor(nom, maladie, argent, poche, etatSante, traitement, pharmacie){
         this.nom = nom;
         this.maladie = maladie;
         this.argent = argent;
         this.poche = poche;
         this.etatSante = etatSante;
         this.traitement = traitement;
+        this.pharmacie = pharmacie;
         this.goTo = () => {
-            pharmacie.push(this.nom);
+            // pharmacie.push(this.nom);
             console.log(`${this.nom} est allé à la pharmacie`);
         };
         this.takeCare = () => {
-            if (this.argent >= 100) {
+            if (this.argent >= this.pharmacie.prix) {
                 console.log(`${this.nom} est soigné`);
             } else {
                 cimetiere.push(this.nom);
@@ -36,23 +44,24 @@ class Malades {
 // |Sangoku|404|80|vide|malade
 // |DarthVader|azmatique|110|vide|malade
 // |Semicolon|syntaxError|60|vide|malade
-let patient1 = new Malades('Marcus', 'mal indenté', 100, 'vide', 'malade');
-let patient2 = new Malades('Optimus', 'unsave', 200, 'vide', 'malade');
-let patient3 = new Malades('Sangoku', 404, 80, 'vide', 'malade');
-let patient4 = new Malades('DarthVader', 'asthmatique', 110, 'vide', 'malade');
-let patient5 = new Malades('Semicolon', 'syntaxError', 60, 'vide', 'malade');
 
-let salleAttente = {
-    nom: "salle d'attente",
-    patients: [patient1, patient2, patient3, patient4, patient5],
-}
-console.log(salleAttente);
-console.log(`Dans la salle d'attente il y a ${salleAttente.patients.length} personnes`);
+let traitement1 = new Pharmacie('ctrl+maj+f', 60);
+let traitement2 = new Pharmacie('saveOnFocusChange', 100);
+let traitement3 = new Pharmacie('CheckLinkRelation', 35);
+let traitement4 = new Pharmacie('Ventoline', 40);
+let traitement5 = new Pharmacie('F12+doc', 20);
+
+
+let patient1 = new Malades('Marcus', 'mal indenté', 100, 'vide', 'malade', "traitement", traitement1);
+let patient2 = new Malades('Optimus', 'unsave', 200, 'vide', 'malade',"traitement", traitement2);
+let patient3 = new Malades('Sangoku', '404', 80, 'vide', 'malade', "traitement", traitement3);
+let patient4 = new Malades('DarthVader', 'azmatique', 110, 'vide', 'malade', "traitement", traitement4);
+let patient5 = new Malades('Semicolon', 'syntaxError', 60, 'vide', 'malade', "traitement", traitement5);
 
 // ## Description du doctor
 // >Le doctor lui reçoit les patients dans son cabinet. Tout d'abord il les diagnostiques puis se fait payer avant de préscrire un traitement. Attention le doctor fait à chaque fois sortir le patient de son cabinet avant de prendre le suivant. Dans son cabinet il y a son chat de race sphynx pour garder un environemment stérile. Son chat miaule toutes les deux secondes dans la console(bonus). La consultation coûte 50€. Les patients son dans un état de traitement avant de sortir du cabinet.
 let doctor = {
-    nom: 'Pierre',
+    nom: 'Doctor',
     tarif: 50,
     cabinet: [],
     diagnostique(patients){
@@ -68,7 +77,12 @@ let doctor = {
 // |nom|argent|cabinet|diagnostique|patienTIn|patientOut
 // |---|---|---|---|---|---|
 // |Debugger|0|[chat]
-
+let salleAttente = {
+    nom: "salle d'attente",
+    patients: [patient1, patient2, patient3, patient4, patient5],
+}
+console.log(salleAttente);
+console.log(`Dans la salle d'attente il y a ${salleAttente.patients.length} personnes`);
 // ### Grille des diagnostiques
 // |maladie|traitement|
 // |---|---|
@@ -80,21 +94,7 @@ let doctor = {
 // ​
 // ## La pharmacie
 // >Les patients iront par après à la pharmacie et recevront leur traitement s'ils ont assez d'argent. Dans le cas ou ils ont assez d'argent ils seront alors en bonne santé sinon ils seront mort et il faudra alors les pousser dans un cimetière.
-class Pharmacie {
-    constructor(traitement, prix){
-        this.traitement = traitement;
-        this.prix = prix; 
-    }
-}
-let traitement1 = new Pharmacie('ctrl+maj+f', 60);
-let traitement2 = new Pharmacie('saveOnFocusChange', 100);
-let traitement3 = new Pharmacie('CheckLinkRelation', 35);
-let traitement4 = new Pharmacie('Ventoline', 40);
-let traitement5 = new Pharmacie('F12+doc', 20);
-
-let pharmacie = [];
 let cimetiere = [];
-
 
 // ### Tarif des traitements
 // |Traitement|prix|
@@ -111,18 +111,7 @@ let cimetiere = [];
 // MARCUS
 doctor.patientIn(patient1);
 console.log(`${patient1.nom} est entré au cabinet`);
-console.log(doctor.cabinet);
-let chat = {
-    contenue : [0],
-    miauler() {
-        setTimeout(()=> {
-            if (this.contenue[0] == 0) {
-                console.log('miaou');
-            }
-        }, 2000)
-    }
-}
-chat.miauler();
+// console.log(doctor.cabinet);
 doctor.diagnostique(patient1);
 console.log(`le traitement de ${patient1.nom} est ${traitement1.traitement}`);
 patient1.paye(doctor);
@@ -131,8 +120,8 @@ console.log(`${patient1.nom} a actuellement ${patient1.argent} euros`);
 doctor.patientOut(patient1);
 console.log(`${patient1.nom} a quitté le cabinet`);
 console.log(doctor.cabinet);
-patient1.goTo(pharmacie);
-console.log(pharmacie);
+patient1.goTo();
+console.log();
 console.log(`le traitement de ${patient1.nom} coûte ${traitement1.prix} euros`);
 patient1.takeCare();
 console.log(`${patient1.nom} est mort`);
@@ -200,10 +189,9 @@ console.log(`${patient5.nom} a quitté le cabinet`);
 patient5.goTo()
 console.log(`le traitement de ${patient5.nom} coûte ${traitement5.prix} euros`);
 patient5.takeCare()
-console.log(`${patient5.nom} est mort`);
 console.log("__________________________________________________");
 salleAttente.patients.splice(salleAttente.patients.indexOf(patient5), 1); console.log(`Dans la salle d'attente il y a ${salleAttente.patients.length} personnes`);
 console.log("__________________________________________________");
-
+console.log(cimetiere);
 
 
